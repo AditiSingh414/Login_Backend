a5bf42f49724fed638e50612e4b5a4ef615ac93c
Aditi Singh <aditi.singh@waisl.in>
2023-05-09 15:27:40 +0530

    updated.

--- src/main/java/com/rit/dto/UserDto.java
+++ src/main/java/com/rit/dto/UserDto.java
@@ -4,6 +4,7 @@ public class UserDto {
 
     private String userName;
     private String password;
+    
 
     public UserDto(String userName, String password) {
         this.userName = userName;
